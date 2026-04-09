7ae7dbb60624f290af1ecc948da165247244cbc2
Андрей Посессор <dronkrasava@MacBook-Pro-Andrej.local>
2026-04-09 14:31:04 +0300

    Замена приветствия-тест

--- src/main/java/io/hexlet/springblog/HomeController.java
+++ src/main/java/io/hexlet/springblog/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Добро пожаловать в Hexlet Spring Blog!";
+        return "Привет! Это мой первый Spring Boot проект!";
     }
 }
